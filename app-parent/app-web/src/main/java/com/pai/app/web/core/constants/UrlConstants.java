fc0f8c41743004830e3dfaa6ab96a729abad67e9
max <max@fuhao>
2017-01-08 14:52:22 +0800

    图

--- app-parent/app-web/src/main/java/com/pai/app/web/core/constants/UrlConstants.java
+++ app-parent/app-web/src/main/java/com/pai/app/web/core/constants/UrlConstants.java
@@ -5,9 +5,4 @@ public class UrlConstants {
 	public final static String MAIN_URL = "/admin/pai/auth/main" + WebConstants.ACTION_POSTFIX;
 	public final static String LOGIN_URL = "/adminLogin" + WebConstants.ACTION_POSTFIX;
 	public final static String LOGIN_FTL = "/admin/pai/auth/login";
-	public final static String MEMBER_LOGIN_URL = "/storefront/member/toLogin" + WebConstants.FRONT_ACTION_POSTFIX;
-	public final static String MEMBER_WAP_LOGIN_URL = "/member/toLogin" + WebConstants.FRONT_ACTION_POSTFIX;
-	public final static String MEMBER_BBSWX_LOGIN_URL = "/bbswx/member/toLogin" + WebConstants.FRONT_ACTION_POSTFIX;
-	public final static String INDEX_URL = "/index" + WebConstants.FRONT_ACTION_POSTFIX;	
-	public final static String VALIDATION_URL = "/storefront/member/toValidate" + WebConstants.FRONT_ACTION_POSTFIX;
 }
